b71ec382168e585b2046c69ee82583914a4f9e56
José Daniel <jdrayman@hotmail.com>
2022-05-09 11:04:40 +0200

    Add files via upload

--- dns.sh
+++ dns.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y install neofetch
 sudo apt install -y git
+echo "vagrant:vagrant" | sudo chpasswd
 #sudo git clone https://github.com/guguidfr/test.git
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo systemctl restart sshd;
