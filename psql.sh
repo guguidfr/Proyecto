22972299f689df674da30432f82ad9be77d34f3e
José Daniel <guguidfr@pm.me>
2022-05-16 15:32:40 +0200

    Delete psql.sh

--- psql.sh
+++
@@ -1,8 +0,0 @@
-#sudo apt update
-#sudo apt -y upgrade
-#sudo apt -y install neofetch
-echo "vagrant:vagrant" | sudo chpasswd
-sudo yum -y install firewalld
-sudo systemctl start firewalld
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo systemctl restart sshd;
