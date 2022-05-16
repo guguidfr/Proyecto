e6e7102272f165a339c9237a7e83c7ce79a29f6c
José Daniel <guguidfr@pm.me>
2022-05-16 15:32:18 +0200

    Delete dns.sh

--- dns.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install neofetch
-sudo apt install -y git
-echo "vagrant:vagrant" | sudo chpasswd
-#sudo git clone https://github.com/guguidfr/test.git
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo systemctl restart sshd;
-#sudo sh /home/vagrant/test/dns/dns-creation.sh
