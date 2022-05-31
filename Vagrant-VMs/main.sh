cd578af71e2778fb1ea068a00b88d1952deddb73
José Daniel <guguidfr@pm.me>
2022-05-31 09:35:19 +0200

    Update main.sh

--- Vagrant-VMs/main.sh
+++ Vagrant-VMs/main.sh
@@ -4,6 +4,7 @@ sudo apt -y install neofetch
 sudo apt install -y git
 echo "vagrant:vagrant" | sudo chpasswd
 git clone https://github.com/guguidfr/get-ansible-debian.git
+git clone https://github.com/guguidfr/Proyecto.git
 sh ./get-ansible-debian/get-ansible.sh
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo systemctl restart sshd;
